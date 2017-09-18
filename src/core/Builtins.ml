f424d002f131514166cfaee7363960f3b163fad0
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-09-18 17:53:50 +0200

    add `false` in builtins

--- src/core/Builtins.ml
+++ src/core/Builtins.ml
@@ -3,14 +3,15 @@ open Solver_types
 module Fmt = CCFormat
 
 let name = "builtins"
-let k_true = Service.Key.make "builtins.true"
+let k_true = Service.Key.makef "%s.true" name
+let k_false = Service.Key.makef "%s.false" name
 
-type term_view += True
+type term_view += True | False
 
 type lemma_view += L_tautology
 
-let[@inline] is_t_true (t:term) = match Term.view t with
-  | True -> true
+let[@inline] is_builtin (t:term) = match Term.view t with
+  | True | False -> true
   | _ -> false
 
 let tcl =
@@ -20,7 +21,7 @@ let tcl =
   in
   {tcl_pp}
 
-let lemma_true_is_true : lemma = Lemma.make L_tautology tcl
+let lemma_trivial : lemma = Lemma.make L_tautology tcl
 
 let build p_id Plugin.S_nil : Plugin.t =
   let module P = struct
@@ -29,16 +30,21 @@ let build p_id Plugin.S_nil : Plugin.t =
 
     let pp out = function
       | True -> Fmt.string out "true"
+      | False -> Fmt.string out "false"
       | _ -> assert false
 
     (* on initialization, add clause [true] *)
-    let init acts t =
-      assert (is_t_true t);
-      let c_true = Clause.make [Term.Bool.pa t] (Lemma lemma_true_is_true) in
-      Actions.push_clause acts c_true
+    let init acts t = match Term.view t with
+      | True ->
+        let c_true = Clause.make [Term.Bool.pa t] (Lemma lemma_trivial) in
+        Actions.push_clause acts c_true
+      | False ->
+        let c = Clause.make [Term.Bool.na t] (Lemma lemma_trivial) in
+        Actions.push_clause acts c
+      | _ -> assert false
 
-    let eval_bool (t:term) : eval_bool_res =
-      assert (is_t_true t);
+    let[@inline] eval_bool (t:term) : eval_bool_res =
+      assert (is_builtin t);
       Eval_unknown
 
     let tc : tc_term = Term.tc_mk ~eval_bool ~init ~pp ()
@@ -46,12 +52,15 @@ let build p_id Plugin.S_nil : Plugin.t =
     (* the main "true" term *)
     let t_true : term = Term.Unsafe.make_term p_id True Type.bool tc
 
+    let t_false : term = Term.Unsafe.make_term p_id False Type.bool tc
+
     let iter_terms yield = yield t_true
     let gc_all () = 0
     let check_if_sat _ = Sat
 
     let provided_services = [
-      Service.Any (k_true, t_true)
+      Service.Any (k_true, t_true);
+      Service.Any (k_false, t_false);
     ]
   end in
   (module P)
--- src/core/Builtins.mli
+++ src/core/Builtins.mli
@@ -5,7 +5,7 @@
 
 open Solver_types
 
-val k_true : term Service.Key.t
-(** Trivial boolean term (and its negation) *)
+val k_true : term Service.Key.t (** Trivial boolean term *)
+val k_false : term Service.Key.t (** Absurd boolean term *)
 
 val plugin : Plugin.Factory.t
