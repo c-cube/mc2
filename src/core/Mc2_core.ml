6d238c3cdd2d7020cdc481db288ba433b74c799f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-09-18 14:54:35 +0200

    add `Mc2_core` module to expose only parts of the core library

---
+++ src/core/Mc2_core.ml
@@ -0,0 +1,25 @@
+
+(** {1 Main Entry Point to mc2} *)
+
+module Atom = Atom
+module Term = Term
+module Type = Type
+module Value = Value
+module Actions = Actions
+module Builtins = Builtins
+module Clause = Clause
+module Proof = Proof
+module Solver = Solver
+module Service = Service
+module Plugin = Plugin
+module Tseitin = Tseitin
+module ID = ID
+module Lemma = Lemma
+
+
+(**/**)
+module Solver_types = Solver_types
+module Util = Util
+module Log = Log
+(**/**)
+
